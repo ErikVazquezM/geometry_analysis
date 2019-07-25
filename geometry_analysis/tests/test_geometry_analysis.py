b923f6db4b726323931a8268b1887f302da016d5
Erik Vazquez <erikvazquezmontelongo@my.unt.edu>
2019-07-25 11:54:38 -0500

    A lot of changes

--- geometry_analysis/tests/test_geometry_analysis.py
+++ geometry_analysis/tests/test_geometry_analysis.py
@@ -7,6 +7,35 @@ import geometry_analysis
 import pytest
 import sys
 
+import numpy as np
+import math
+
 def test_geometry_analysis_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "geometry_analysis" in sys.modules
+
+def test_calculate_distance():
+    """Test the calculate_distance function"""
+
+    r1 = np.array([0,0,-1])
+    r2 = np.array([0,1,0])
+
+    expected_distance = np.sqrt(2)
+
+    calculated_distance = geometry_analysis.calculate_distance(r1, r2)
+
+    assert expected_distance == calculated_distance
+
+def test_calculate_angle():
+    """Test the calculate_distance function"""
+
+    r1 = np.array([-1,0,0])
+    r2 = np.array([0,0,0])
+    r3 = np.array([1,0,0])
+
+    expected_theta = math.pi
+
+    calculated_theta = geometry_analysis.calculate_angle(r1, r2, r3)
+
+    assert expected_theta == calculated_theta
+
