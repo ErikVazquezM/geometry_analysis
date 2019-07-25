9f68626b74394521fb86a5c85fe70c32b6bdfa63
Erik Vazquez <erikvazquezmontelongo@my.unt.edu>
2019-07-25 13:41:35 -0500

    More tests

--- geometry_analysis/tests/test_geometry_analysis.py
+++ geometry_analysis/tests/test_geometry_analysis.py
@@ -10,6 +10,32 @@ import sys
 import numpy as np
 import math
 
+@pytest.fixture()
+def water_molecule():
+    name = "water"
+    symbols = ["H", "O", "H"]
+    coordinates = np.array([[2,0,0], [0,0,0], [-2,0,0]])
+
+    water = geometry_analysis.Molecule(name, symbols, coordinates)
+    
+    return water
+
+def test_molecules_set_coordinates(water_molecule):
+    """Test bond list is rebuilt when we reset coordinates """
+
+    num_bonds = len(water_molecule.bonds)
+
+    assert num_bonds == 2
+
+    new_coordinates = np.array([[5,0,0], [0,0,0], [-2,0,0]])
+
+    water_molecule.coordinates = new_coordinates
+
+    new_bonds = len(water_molecule.bonds)
+
+    assert new_bonds == 1
+    assert np.array_equal(new_coordinates, water_molecule.coordinates)
+
 def test_geometry_analysis_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "geometry_analysis" in sys.modules
@@ -26,7 +52,7 @@ def test_calculate_distance():
 
     assert expected_distance == calculated_distance
 
-def test_calculate_angle():
+def test_calculate_angle_180():
     """Test the calculate_distance function"""
 
     r1 = np.array([-1,0,0])
@@ -38,4 +64,25 @@ def test_calculate_angle():
     calculated_theta = geometry_analysis.calculate_angle(r1, r2, r3)
 
     assert expected_theta == calculated_theta
+
+def test_calculate_angle_90():
+    """Test the calculate_distance function"""
+
+    r1 = np.array([1,0,0])
+    r2 = np.array([0,0,0])
+    r3 = np.array([0,1,0])
+
+    expected_theta = (math.pi) / 2
+
+    calculated_theta = geometry_analysis.calculate_angle(r1, r2, r3)
+
+    assert expected_theta == calculated_theta
+
+@pytest.mark.parametrize('p1, p2, p3, expected_angle', [
+    (np.array([-1,0,0]), np.array([0,0,0]), np.array([1,0,0]), 180),
+    (np.array([1,0,0]), np.array([0,0,0]), np.array([0,1,0]), 90),
+    ])
+def test_calculate_angle(p1, p2, p3, expected_angle):
+    calculated_theta = geometry_analysis.calculate_angle(p1, p2, p3, expected_angle)
+    assert expected_angle == calculated_theta
 
